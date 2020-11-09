ce3ac4a739c1d3e922741e942434f12ff599236a
HANYA3106 <65996959+HANYA3106@users.noreply.github.com>
2020-11-09 11:27:45 +0900

    mini_magick変更２

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,9 +11,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 #     storage :fog
 #   end
 #   # Choose what kind of storage to use for this uploader:
-#   # storage :file
-#   # storage :fog
+  storage :file
+  # storage :fog
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 #   # Override the directory where uploaded files will be stored.
 #   # This is a sensible default for uploaders that are meant to be mounted:
 #   def store_dir
@@ -33,18 +36,20 @@ class ImageUploader < CarrierWave::Uploader::Base
 #   #
 #   # def scale(width, height)
 #   #   # do something
-#   # end
-
+#   # end 
+  version :medium do
+    process resize_to_fill: [1080, 1080]
+  end
 #   # Create different versions of your uploaded files:
 #   # version :thumb do
-    process resize_to_limit: [700, 700]
+    # process resize_to_limit: [700, 700]
 #   # end
 
 #   # Add a white list of extensions which are allowed to be uploaded.
 #   # For images you might use something like this:
-#   # def extension_whitelist
-#   #   %w(jpg jpeg gif png)
-#   # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
 #   # Override the filename of the uploaded files:
 #   # Avoid using model.id or version_name here, see uploader/store.rb for details.
@@ -52,13 +57,4 @@ class ImageUploader < CarrierWave::Uploader::Base
 #   #   "something.jpg" if original_filename
 #   # end
 # end
-  process :convert => 'png'
-
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
-  def filename
-    super.chomp(File.extname(super)) + '.png' if original_filename.present?
-  end
 end
